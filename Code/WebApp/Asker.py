6c88177d00df43bab5f0a6d5d65c8e0ea453b796
Lacuisse <frederic.assmus@gmail.com>
2020-12-03 18:03:21 +0100

    Fini question types et réglé pb de sujet absent (plus d'erreur sur webapp)

--- Code/WebApp/Asker.py
+++ Code/WebApp/Asker.py
@@ -117,7 +117,7 @@ class ask:
             subject_dict['subject'] = ents_list[-1] #on renvoie la dernière entité nommée pertinente trouvée
             for other_noun in nouns_list : 
                 subject_dict['infos'].append(other_noun)
-            return(subject_dict)
+            #return(subject_dict)
             
     
         else : 
@@ -131,12 +131,14 @@ class ask:
             #print(dep_list)
             if(len(nouns_list)) == 0 :
                 print("subject not found, please try another formulation", '\n')
+
             else :
                 print("subject found by noun: " + str(nouns_list[-1]), '\n')
                 subject_dict['subject'] = str(nouns_list[-1]) #le sujet est le dernier chunk
                 for other_noun in nouns_list[0:-1] : #dans ces cas de figure avec + d'un nom, il faudra quand même récupérer le nom qui n'est pas le sujet, pour aller l'utiliser en scrappant la page wiki du sujet
                     subject_dict['infos'].append(other_noun)
-                return(subject_dict)
+                #return(subject_dict)
+        return(subject_dict)
 
     def get_sections_list(self, page):
         osef_list = ['Sources', 'Further reading', 'External links']
@@ -166,29 +168,153 @@ class ask:
         return paragraph
 
     def get_best_answer(self, question, subject):
-        page = self.wiki.page(wikipedia.search(subject)[0])
 
-        paragraph = self.get_paragraph(page)
+        search = wikipedia.search(subject)
+        if len(search) > 0 : 
+            page = self.wiki.page(wikipedia.search(subject)[0])
+
+            paragraph = self.get_paragraph(page)
         
         
-        answers = []
-        scores = []
+            answers = []
+            scores = []
 
-        for p in paragraph[:15]:
-            answer, score = self.generateAnswer(question, p)
-            answers.append(answer)
-            scores.append(score)
+            for p in paragraph[:15]:
+                answer, score = self.generateAnswer(question, p)
+                answers.append(answer)
+                scores.append(score)
             
-        max_value = max(scores)
+            max_value = max(scores)
         
-        index = scores.index(max_value)
-        return answers[index], paragraph[index], page.fullurl
+            index = scores.index(max_value)
+            return answers[index], paragraph[index], page.fullurl
+        else : 
+        	   return("Your question doesn't seem to correspond to a specific subject. Could you try to reformulate ?", "No Wikipedia page found", "")
+
+    def is_wh (self, question) : 
+        doc = self.nlp(question)
+        interrogatives = ['where', 'who','why','what','when','which','how', 'Where', 'Who','Why','What','When','Which', 'How']
+        words = [word for word in doc]
+        if words[0].pos_ == 'SPACE' : #si le premier token est un espace, on le supprime pour le traitement
+            words = words[1:]
+    
+        if words[0].text in interrogatives or words[1].text in interrogatives : #si le premier ou deuxième mot de la question est un mot interrogatif, c'est une question en "wh-" : ex "When is Barack Obama born?", "In which country is Paris ?"
+            return(True)
+        else : 
+            return(False)     
+
+    def is_polar(self, question) : #questions yes/no
+        doc = self.nlp(question)
+        words = [word for word in doc]
+        if words[0].pos_ == 'SPACE' : 
+            words = words[1:]
+    
+        if words[0].pos_ == 'AUX' : #si la question commence par un auxiliaire, c'est une question yes/no
+            return(True)
+        else : 
+            return(False)
+
+    def is_pseudocleft (self, question) : #détecter les pseudocleft -> phrases qui ressemblent à des questions mais n'en sont pas : "Who is the President of Nicaragua doesn't interest me."    
+        doc = self.nlp(question) 
+        interrogatives = ['where', 'who','why','what','when','which','how', 'Where', 'Who','Why','What','When','Which', 'How']
+        words = [word for word in doc]
+        if words[0].pos_ == 'SPACE' : 
+            words = words[1:]
+    
+        if words[0].text in interrogatives and words[0].head.dep_ in ["csubj", "advcl"] : #si la dépendence head du mot interrogatif est un adverbial clause ou un subject clause (en gros le sujet ou l'adverbe sont des clauses, genre le sujet de la question c'est le "What", comme dans la phrase "What she says is true"), alors on est face à un pseudocleft (ou pseudo-wh) -> c'est une règle linguistique        
+            return(True) 
+        else : 
+            return(False)
+
+    def is_tutorial (self, question) : #détecter les questions tutorielles
+        doc = self.nlp(question)
+        modals = ['to', 'can', 'could', 'shall', 'should']
+        words = [word for word in doc]
+        if words[0].text == "How" and words[1].text in modals  : 
+            return(True)
+        else : 
+            return(False)
+    
+    def is_toapp (self, question) : #détecte les questions adressées à l'app
+        doc = self.nlp(question)
+        words = [word for word in doc]
+        toapp_words = ['you', 'your', 'You', 'Your']
+        is_toapp = False
+        if doc.ents == () : 
+            for word in words : 
+                if word.text in toapp_words : 
+                    is_toapp = True
+        else : 
+            for ent in doc.ents : 
+                for word in words : 
+                    if word.text in toapp_words : 
+                        is_toapp = True
+        return(is_toapp)
+
+    def is_personal (self, question) : 
+        doc= self.nlp(question)
+        words = [word for word in doc]
+        personal_words = ['I', 'i', 'My', 'my', 'Me', 'me', 'Myself', 'myself']
+        is_personal = False
+        if doc.ents == () : 
+            for word in words : 
+                if word.text in personal_words : 
+                    is_personal = True
+        else : 
+            for ent in doc.ents : 
+                for word in words : 
+                    if word.text in personal_words : 
+                        is_personal = True
+        return(is_personal)
+
+    def predifined_answer (self, question) : #to compute the predifined answer given the question type
         
+        if self.is_polar(question) :
+            sujet = self.extract_subject_with_spacy(question)
+            a, p, u = self.get_best_answer(question, sujet['subject'])
+            
+            if not u : 
+                u = "Sorry, we couldn't find any Wikipedia article related to your question."
+            return(True, "You are expecting me to answer by yes or non. I can’t give an answer that is that precise. But here is information that will help you answer it yourself.",
+                   "Maybe you should check that Wikipedia link.",
+                   u )
+        elif self.is_pseudocleft(question) :
+            sujet = self.extract_subject_with_spacy(question)
+            a, p, u = self.get_best_answer(question, sujet['subject'])
+            if not u : 
+                u = "Sorry, we couldn't find any Wikipedia article related to your question."
+            return(True,"What you just typed looks like a question… but doesn’t seem to be one. Maybe you should try to rephrase it ?",
+                   "But this Wikipedia page could still help you.",
+                   u )
+        elif self.is_tutorial(question) : 
+            return(True, "It seems like you are asking for a tutorial. I can’t provide such information to you… maybe you should try to rephrase your question, or ask a more factual one.",
+                   "If you really want a good tutorial, you can check on Wikihow",
+                   "https://www.wikihow.com/Main-Page")
+        elif self.is_personal(question) : 
+            return(True, 'I can’t answer personal questions… Maybe you should try finding it by yourself. You can still ask me factual questions, and I’ll be glad to answer !', 
+                  "Really, I can't.", 
+                  "https://www.wikihow.com/Know-Your-True-Self") 
+        elif self.is_toapp(question) : 
+            return(True, "Are you trying to get information that concerns me ? I’m shy, so I won’t answer, sorry … Maybe you should try asking some general knowledge questions !",
+                   "If you want to know more about me or my creators :",
+                   "LIEN VERS LE ABOUT US")
+        else : 
+            return(False, None, None, None)
+
 
     def run(self, question):
-        sujet = self.extract_subject_with_spacy(question)
-        a, p, u = self.get_best_answer(question, sujet['subject'])
-        return a, p, u
+        isSpecial, specialAnswer, details, url = self.predifined_answer(question)
+        if isSpecial : 
+            return(specialAnswer, details, url)    
+        else : 
+            sujet = self.extract_subject_with_spacy(question)
+            if sujet['subject'] == "": 
+                return("Your question doesn't seem to correspond to a specific subject. Could you try to reformulate ?", "No Wikipedia page found","")
+            else: 
+                a, p, u = self.get_best_answer(question, sujet['subject'])
+                return a, p, u
+
+
         
         
 
